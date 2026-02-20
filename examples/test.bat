5e7893cde2f7b3873c57f43a44a47f2ab4f9f6f6
Sulong Chen <sulong@jmxbrands.com>
2026-02-20 18:24:22 +0800

    docs(examples): 添加批处理文件以测试一些常见用法
    该文件包含多个示例命令，展示如何使用不同的输入格式、输出格式和转换选项。

---
+++ examples/test.bat
@@ -0,0 +1,8 @@
+dotnet run ../examples/5_star_base.jpg d:\out1.gif
+dotnet run ../examples/Amish-Noka-Dresser.jpg d:\out2.bmp
+dotnet run ../examples/progressive.jpg d:\out3.png
+dotnet run ../examples/video-001.cmyk.jpeg d:\out4.webp
+dotnet run ../examples/5_star_base.jpg d:\out5.bmp --gray
+dotnet run d:\out5.bmp d:\out6.jpg
+dotnet run ../examples/Amish-Noka-Dresser.jpg d:\out7.png resizefit:200x200
+dotnet run ../examples/video-001.cmyk.jpeg d:\out8.webp resizefit:1000x1000
